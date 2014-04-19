219cc8d565c52e99c879b1b6141c98e063717fcc
James Adams <james@makenub.com>
2014-04-18 22:10:06 -0600

    Added car ignition view controller

---
+++ BLECarTesting/MNCarIgnitionViewController.h
@@ -0,0 +1,13 @@
+//
+//  MNUserTabIgnitionViewController.h
+//  BLECarTesting
+//
+//  Created by James Adams on 4/18/14.
+//  Copyright (c) 2014 JamesAdams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MNCarIgnitionViewController : UIViewController
+
+@end
